03849caf70b0f5d00ea185785521209adba33b5c
Szybet <53944559+Szybet@users.noreply.github.com>
2025-03-25 14:20:49 +0100

    Cleaned up a bit

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,13 @@ fn main() -> eframe::Result {
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0])
-            .with_icon(
-                // NOTE: Adding an icon is optional
-                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
-                    .expect("Failed to load icon"),
-            ),
+            .with_min_inner_size([300.0, 220.0]),
         ..Default::default()
     };
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(fizcalczaok_gui::FizCalcApp::new(cc)))),
     )
 }
 
@@ -50,7 +45,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(fizcalczaok_gui::FizCalcApp::new(cc)))),
             )
             .await;
 
@@ -62,7 +57,7 @@ fn main() {
                 }
                 Err(e) => {
                     loading_text.set_inner_html(
-                        "<p> The app has crashed. See the developer console for details. </p>",
+                        "<p> The app has crashed. See the developer console for details. ALSO FOLLOW THE INSTRUCTIONS</p>",
                     );
                     panic!("Failed to start eframe: {e:?}");
                 }
