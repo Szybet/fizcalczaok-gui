15c70b3c41e1807c35953416ab835db1afb162fb
Szybet <53944559+Szybet@users.noreply.github.com>
2025-03-26 18:38:00 +0100

    finish

--- src/app.rs
+++ src/app.rs
@@ -1,16 +1,22 @@
+use fizcalczaok::{oblicz, potegowanie_str_wrapper, stworz_dzial};
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct FizCalcApp {
-    label: String,
-    value: f32,
+    main_calc: String,
+    main_calc_answer: String,
+    single_calc: String,
+    single_calc_answer: String,
 }
 
 impl Default for FizCalcApp {
     fn default() -> Self {
         Self {
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            main_calc: String::new(),
+            single_calc: String::new(),
+            main_calc_answer: String::new(),
+            single_calc_answer: String::new(),
         }
     }
 }
@@ -31,20 +37,46 @@ impl eframe::App for FizCalcApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Fizcalczaok");
-            ui.label("Już nie pamietam co ten skrót znaczy");
+            ui.label("Kalkulator, zgodny z regułami zaokrągleń w działaniach arytmetycznych laboratorium fizyki na PO");
+            ui.label(
+                format!("{}\n{}\n{}\n{}\n{}", "- Nie ma kolejności wykonywania działań, od lewej do prawej",
+                "- Nie mieszaj działań dodawania, odejmowania z działań z mnożeniem, dzieleniem",
+                "- Poprawność działań, na podstawie przykładów z poradnika pdf zostały zaimplementowane tu na podstawie testów: https://github.com/Szybet/fizcalczaok",
+                "- By zrobić np pierwiastek 3 stopnia, użyj potęgi 1/3 czyli 0.3333333...",
+                "- By zaznaczyć że liczba jest liczbą dokładną, należy użyć znak \"d\" przed nim, np: d5",
+            ));
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
+            if ui.button("Sprzątanie").clicked() {
+                self.main_calc = "".to_string();
+                self.main_calc_answer = "".to_string();
+                self.single_calc = "".to_string();
+                self.single_calc_answer = "".to_string();
             }
+
+            ui.label("Podaj działanie: (dodawanie: +, odejmowanie: -, mnożenie: *, dzielenie: /)");
+            ui.horizontal(|ui| {
+                ui.text_edit_multiline(&mut self.main_calc);
+                if ui.button("Oblicz").clicked() {
+                    let d = stworz_dzial(&self.main_calc);
+                    let o = oblicz(d.clone());
+                    self.main_calc_answer = o.to_string();             
+                }
+            });
+            ui.label(format!("Wynik: {}", self.main_calc_answer));
 
             ui.separator();
 
+            ui.label("Podaj działanie pojedyńcze (potęgowanie: ^): ");
+            ui.horizontal(|ui| {
+                ui.text_edit_multiline(&mut self.single_calc);
+                if ui.button("Oblicz").clicked() {
+                    let d = potegowanie_str_wrapper(self.single_calc.clone());
+                    self.single_calc_answer = d.to_string();             
+                }
+            });
+            ui.label(format!("Wynik: {}", self.single_calc_answer));
+            
+            ui.separator();
         });
     }
 }
